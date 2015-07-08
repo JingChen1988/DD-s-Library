bb1204e0d00117a05033e83a7afa77adaa291bae
DongDong <dongdong19881019@163.com>
2015-07-08 17:44:48 +0800

    第1次：初始化项目，进行测试
    Signed-off-by: DongDong <dongdong19881019@163.com>

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    string txt = "This is DD's Library\n" +
+        "Add Tag DD & Layer DD\n" +
+        "Screen 480*768\n";
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(txt);
+    }
+}
