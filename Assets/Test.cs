ee6fa4336f357750d6b053eccd1a534f451b0e35
DongDong <dongdong19881019@163.com>
2015-07-10 00:40:41 +0800

    家里同步 7-9

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,7 +5,8 @@ public class Test : MonoBehaviour
 {
     string txt = "This is DD's Library\n" +
         "Add Tag DD & Layer DD\n" +
-        "Screen 480*768\n";
+        "Screen 480*768\n"+
+        "Home Update 7-9";
 
     void Start()
     {
